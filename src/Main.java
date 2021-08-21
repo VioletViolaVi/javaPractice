e7adcd9fd7e79cb53b1dbaf8c7f0c69c64f07cc2
VioletViola <vivian.v18723@gmail.com>
2021-08-21 19:51:26 +0100

    Practice creating calculations via calling out methods.

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,13 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 
+        // ternary operator usage
+        int time = 2;
+        String greeting = time < 18 ? "Good day.": "Good evening.";
+        System.out.println(greeting);
+        System.out.println((time < 18) ? "Good day.": "Good evening.");
+
         // finding specific number in array
-        System.out.println("Challenge 1:");
         FindTheNum findTheNum = new FindTheNum();
         System.out.println(findTheNum.numThatIsPassed);
         Arrays.sort(findTheNum.arrayOfNums);
@@ -12,17 +17,29 @@ public class Main {
         System.out.println(Arrays.binarySearch(findTheNum.arrayOfNums, findTheNum.numThatIsPassed));
         System.out.println(Arrays.binarySearch(findTheNum.arrayOfNums, findTheNum.numThatIsPassed) > -1);
 
-        // printing your full name
-        System.out.println("Challenge 2:");
+        /* https://www.w3resource.com/java-exercises/basic/index.php */
+
+        // 1) printing your full name
+        System.out.println("Challenge 1:");
         PrintName printName = new PrintName();
         printName.myName("Jessie J");
 
-        // ternary operator usage
-        int time = 2;
-        String greeting = time < 18 ? "Good day.": "Good evening.";
-        System.out.println(greeting);
-        System.out.println((time < 18) ? "Good day.": "Good evening.");
+        // 2) adding numbers
+        System.out.println("Challenge 2:");
+        Math math = new Math();
+        System.out.println(math.add(3,3));
 
-        //
+        // 3) dividing numbers
+        System.out.println("Challenge 3:");
+        System.out.println(math.divide(55, 11));
+
+        // 4) multiplying numbers
+        System.out.println("Challenge 4:");
+        System.out.println(math.multiply(5, 5));
+
+        // 4a) -5 + 8 * 6
+        System.out.println("Challenge 4a:");
+        System.out.println(math.multiply(math.add(-5, 8) , 6));
+
     }
 }
---
+++ src/Math.java
@@ -0,0 +1,17 @@
+public class Math {
+    public int add(int num1, int num2) {
+        return num1 + num2;
+    }
+    public int divide(int num1, int num2) {
+        return num1 / num2;
+    }
+    public int multiply(int num1, int num2) {
+        return num1 * num2;
+    }
+    public int subtract(int num1, int num2) {
+        return num1 - num2;
+    }
+    public int modulo(int num1, int num2) {
+        return num1 % num2;
+    }
+}
