9aa0e898ba573d45fdd6cc79fa05b9d1615c8f4b
VioletViola <vivian.v18723@gmail.com>
2021-08-21 17:23:29 +0100

    Practice printing greeting and name via classes.

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,22 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
+
         // finding specific number in array
+        System.out.println("Challenge 1:");
         FindTheNum findTheNum = new FindTheNum();
         System.out.println(findTheNum.numThatIsPassed);
         Arrays.sort(findTheNum.arrayOfNums);
         System.out.println(Arrays.toString(findTheNum.arrayOfNums));
         System.out.println(Arrays.binarySearch(findTheNum.arrayOfNums, findTheNum.numThatIsPassed));
         System.out.println(Arrays.binarySearch(findTheNum.arrayOfNums, findTheNum.numThatIsPassed) > -1);
+
+        // printing your full name
+        System.out.println("Challenge 2:");
+        PrintName printName = new PrintName();
+        printName.myName("Jessie J");
+
+
+
     }
 }
---
+++ src/PrintName.java
@@ -0,0 +1,6 @@
+public class PrintName {
+    public void myName(String fullName) {
+        System.out.println("Hello");
+        System.out.println("My name is " + fullName);
+    }
+}
